b34b6a4207e9e3f4a8c2becb1e19f1fc1bef41cc
projecteinf <projecteinf@boscdelacoma.cat>
2026-05-15 12:04:15 +0200

    Aplicació Hello World exemple manual

---
+++ Applications/HelloWorld/mini-api/src/index.ts
@@ -0,0 +1,13 @@
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionant correctament" });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escoltant a http://localhost:${port}`);
+});
