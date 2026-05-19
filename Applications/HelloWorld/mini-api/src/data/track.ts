6cbc121778736a8654970cca0d56070f55d777de
projecteinf <projecteinf@boscdelacoma.cat>
2026-05-19 09:17:51 +0200

    Afegir tracks com a tipus i dades a la miniapi

---
+++ Applications/HelloWorld/mini-api/src/data/track.ts
@@ -0,0 +1,22 @@
+import { Track } from "../types/track";
+
+export const tracks: Track[] = [
+  {
+    id: "trk-0001",
+    title: "Blinding Lights",
+    artist: "The Weeknd",
+    duration: 200
+  },
+  {
+    id: "trk-0002",
+    title: "Shape of You",
+    artist: "Ed Sheeran",
+    duration: 233
+  },
+  {
+    id: "trk-0003",
+    title: "Levitating",
+    artist: "Dua Lipa",
+    duration: 203
+  }
+];
