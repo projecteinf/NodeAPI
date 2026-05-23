42c76c53a831d40177586d1c097f4487fa5a1ffc
projecteinf <projecteinf@boscdelacoma.cat>
2026-05-23 13:04:17 +0200

    script per instal·lar docker a Debian

---
+++ install_docker.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
+docker ps
+sudo docker ps
+grep docker /etc/group
+sudo usermod -aG docker isard
+groups isard
